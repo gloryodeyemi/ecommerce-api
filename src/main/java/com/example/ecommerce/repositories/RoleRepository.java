4f373b50175b9b068f04c8032a3cc97493ddd069
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-03-22 22:41:58 +0100

    password encoder

---
+++ src/main/java/com/example/ecommerce/repositories/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.example.ecommerce.repositories;
+
+import com.example.ecommerce.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(Role name);
+}
--- src/main/java/com/example/ecommerce/repositories/UserRepository.java
+++ src/main/java/com/example/ecommerce/repositories/UserRepository.java
@@ -1,2 +1,13 @@
-package com.example.ecommerce.repositories;public interface UserRepository {
+package com.example.ecommerce.repositories;
+
+import com.example.ecommerce.models.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserAccount, Long> {
+    Optional<UserAccount> findByEmail(String emailAddress);
+    Boolean existsByEmail(String emailAddress);
 }
