dc012dc49eb3002fc0bd5bc8945fa9e66c953aa3
Glory Odeyemi <gloryodeyemi.nike@gmail.com>
2022-03-24 02:12:08 +0100

    list cart items

--- src/main/java/com/example/ecommerce/repositories/CartRepository.java
+++ src/main/java/com/example/ecommerce/repositories/CartRepository.java
@@ -4,6 +4,10 @@ import com.example.ecommerce.models.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    List<Cart> findAllByUserId(Long userId);
 }
---
+++ src/main/java/com/example/ecommerce/services/CartService.java
@@ -0,0 +1,59 @@
+package com.example.ecommerce.services;
+
+import com.example.ecommerce.dtos.AddToCartDto;
+import com.example.ecommerce.dtos.CartDto;
+import com.example.ecommerce.dtos.CategoryDto;
+import com.example.ecommerce.exceptions.UserAlreadyExistException;
+import com.example.ecommerce.models.*;
+import com.example.ecommerce.repositories.CartRepository;
+import com.example.ecommerce.repositories.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CartService {
+    @Autowired
+    CartRepository cartRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    public ResponseEntity<Cart> addToCart(AddToCartDto addToCartDto, Long userId) throws UserAlreadyExistException {
+        // checking to see if user is registered
+        Optional<UserAccount> user = userRepository.findById(userId);
+        if (user.isPresent()) {
+            // checking to see if user is a customer
+            if (user.get().getRole().getName().equals("Customer")) {
+                // create a new cart object
+                Cart cart = new Cart();
+                BeanUtils.copyProperties(addToCartDto, cart);
+                return ResponseEntity.ok(cartRepository.save(cart));
+            }
+            throw new UserAlreadyExistException("Action not allowed!");
+        }
+        throw new UserAlreadyExistException("Unauthorized!");
+    }
+
+    public ResponseEntity<CartCost> listCartItems(Long userId) throws UserAlreadyExistException {
+       List<Cart> cartList = cartRepository.findAllByUserId(userId);
+       List<CartDto> cartItems = new ArrayList<>();
+       for (Cart cart: cartList){
+            CartDto cartDto = new CartDto();
+            cartItems.add(cartDto);
+       }
+       Double totalCost = 0D;
+       for (CartDto cartDto:cartItems){
+           totalCost += (cartDto.getProduct().getPrice() * cartDto.getQuantity());
+       }
+       CartCost cartCost = new CartCost();
+       cartCost.setCartItems(cartItems);
+       cartCost.setTotalCost(totalCost);
+       return ResponseEntity.ok(cartCost);
+    }
+}
